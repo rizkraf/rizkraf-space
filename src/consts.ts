9ae50a0f8b0adfe40647746ab2e7282f2d950f73
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2025-09-03 12:46:12 +0700

    chore: update dependencies and add new components
    - Updated dependencies in package.json for various Astro packages and added Tailwind CSS.
    - Created a new Clock component to display the current time in Jakarta timezone.
    - Added a Description component to introduce the author.
    - Refactored Header component to simplify the layout and removed unnecessary links.
    - Introduced a SocialLink component to display social media links and email.
    - Updated consts.ts to reflect the author's name and added social links.
    - Modified index.astro to include new components and improve layout.
    - Cleaned up global.css by removing old styles and integrating Tailwind CSS.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,20 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
+export const SITE_TITLE = "Rizky Rafi Azhara";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SOCIAL_LINKS = [
+  {
+    label: "Email",
+    href: "mailto:rizkyrafiazh@gmail.com",
+    value: "rizkyrafiazh@gmail.com"
+  },
+  {
+    label: "Github",
+    href: "https://github.com/rizkraf"
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/rizkraf"
+  }
+]
